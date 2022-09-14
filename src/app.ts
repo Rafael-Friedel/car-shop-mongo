4a4555a28c1b99169237979377ca0e0b2ae9a8ae
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 16:06:06 -0300

    feat: implementando rota de car no app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import errorHandler from './middlewares/errorHandler';
+import routerCar from './routes/Car.route';
 
 const app = express();
 app.use(express.json());
+app.use(routerCar);
 app.use(errorHandler);
 
 export default app;
