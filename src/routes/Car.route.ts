ef4a35439e7b9d140cee9145331c68a46dd687e7
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 17:24:47 -0300

    fix: corrigindo rotas

--- src/routes/Car.route.ts
+++ src/routes/Car.route.ts
@@ -9,10 +9,10 @@ const car = new CarModel();
 const carService = new CarService(car);
 const carController = new CarController(carService);
 
-route.post('/car', (req, res) => carController.create(req, res));
-route.get('/car/:id', (req, res) => carController.readOne(req, res));
-route.get('/car', (req, res) => carController.read(req, res));
-route.delete('/car/:id', (req, res) => carController.delete(req, res));
-route.put('/car/:id', (req, res) => carController.update(req, res));
+route.post('/cars', (req, res) => carController.create(req, res));
+route.get('/cars/:id', (req, res) => carController.readOne(req, res));
+route.get('/cars', (req, res) => carController.read(req, res));
+route.delete('/cars/:id', (req, res) => carController.delete(req, res));
+route.put('/cars/:id', (req, res) => carController.update(req, res));
 
 export default route;
