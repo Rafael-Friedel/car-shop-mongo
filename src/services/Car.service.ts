3bc67c0c7f5dafca71696baa6d5ef6e2a3762a56
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 17:40:57 -0300

    fix: corrigindo sintaxe

--- src/services/Car.service.ts
+++ src/services/Car.service.ts
@@ -4,10 +4,10 @@ import { IModel } from '../interfaces/IModel';
 import { IService } from '../interfaces/IService';
 
 export default class CarService implements IService<ICar> {
-  private _frame: IModel<ICar>;
+  private _car: IModel<ICar>;
 
   constructor(model: IModel<ICar>) {
-    this._frame = model;
+    this._car = model;
   }
 
   public async create(obj: ICar): Promise<ICar> {
@@ -15,27 +15,27 @@ export default class CarService implements IService<ICar> {
     if (!parsed.success) {
       throw parsed.error;
     }
-    return this._frame.create(parsed.data as ICar);
+    return this._car.create(parsed.data as ICar);
   }
 
   public async readOne(_id: string): Promise<ICar> {
-    const frame = await this._frame.readOne(_id);
-    if (!frame) throw new Error(ErrorTypes.EntityNotFound);
-    return frame;
+    const car = await this._car.readOne(_id);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
   }
 
   public async read(): Promise<ICar[]> {
-    return this._frame.read();
+    return this._car.read();
   }
 
   public async delete(_id: string): Promise<ICar> {
-    const deleted = await this._frame.delete(_id);
+    const deleted = await this._car.delete(_id);
     if (!deleted) throw new Error(ErrorTypes.EntityNotFound);
     return deleted;
   }
 
   public async update(_id: string, obj: ICar): Promise<ICar> {
-    const updated = await this._frame.update(_id, obj);
+    const updated = await this._car.update(_id, obj);
     if (!updated) throw new Error(ErrorTypes.EntityNotFound);
     return updated;
   }
