4ef436fe8f1fa0ab1ccf1acc59d3f6646675a085
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 14:40:04 -0300

    inittial_commit: update req 1, iniciando projeto, criando interface Imodel

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,9 @@
+interface IModel<T> {
+  create(obj: T): Promise<T>;
+  readOne(_id: string): Promise<T | null>;
+  read(): Promise<T[]>;
+  update(_id: string, obj: T): Promise<T | null>;
+  delete(_id: string): Promise<T | null>;
+}
+
+export default IModel;
