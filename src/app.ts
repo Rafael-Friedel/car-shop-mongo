0389194d4e3d99e5b02f199440d517a32dde1e10
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 17:56:57 -0300

    fix: corrigindo rotas

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/errorHandler';
 import routerCar from './routes/Car.route';
+import routerMotorcycle from './routes/Motorcycle.route';
 
 const app = express();
 app.use(express.json());
-app.use(routerCar);
+app.use('/cars', routerCar);
+app.use('/motorcycles', routerMotorcycle);
 app.use(errorHandler);
 
 export default app;
