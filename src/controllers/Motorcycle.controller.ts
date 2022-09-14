cbe46bf1e7313e7157a31b56b2c9aaf30cb8a6cf
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 18:44:22 -0300

    feat: implementando regra de negocio na rota de motorcycle create

--- src/controllers/Motorcycle.controller.ts
+++ src/controllers/Motorcycle.controller.ts
@@ -5,7 +5,10 @@ import { IService } from '../interfaces/IService';
 export default class MotorcycleController {
   constructor(private _service: IService<IMotorcycle>) {}
 
-  public async create(req: Request, res: Response<IMotorcycle>) {
+  public async create(
+    req: Request & { body: IMotorcycle },
+    res: Response<IMotorcycle>,
+  ) {
     const results = await this._service.create(req.body);
     return res.status(201).json(results);
   }
