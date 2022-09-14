8c1fb8d7e75b4528670027c06bc7dcba8d768a4c
Rafael <RafaFriedelDev@outlook.com>
2022-09-14 18:10:40 -0300

    fix: corrigindo status

--- src/errors/catalog.ts
+++ src/errors/catalog.ts
@@ -8,17 +8,14 @@ type ErrorResponseObject = {
   httpStatus: number;
 };
 
-// aqui o tipo do catálogo
 export type ErrorCatalog = {
-  // onde cada chave desse objeto é uma chave do Enum ErrorTypes
-  // e cada valor é um objeto de resposta da API
   [key in ErrorTypes]: ErrorResponseObject;
 };
 
 export const errorCatalog: ErrorCatalog = {
   EntityNotFound: {
     message: 'Entity not found',
-    httpStatus: 400,
+    httpStatus: 404,
   },
   InvalidMongoId: {
     message: 'Id must be a 24 characters hexadecimal',
