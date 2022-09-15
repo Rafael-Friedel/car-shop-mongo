bdb403fc8238da37049baf0e5216879b38cd6516
Rafael <RafaFriedelDev@outlook.com>
2022-09-15 11:02:56 -0300

    feat: implementando regra de negócio

--- src/models/Car.model.ts
+++ src/models/Car.model.ts
@@ -2,15 +2,18 @@ import { model as mongooseCreateModel, Schema } from 'mongoose';
 import { ICar } from '../interfaces/ICar';
 import MongoModel from './Mongo.model';
 
-const carMongooseSchema = new Schema<ICar>({
-  status: Boolean,
-  model: String,
-  year: Number,
-  color: String,
-  buyValue: Number,
-  doorsQty: Number,
-  seatsQty: Number,
-});
+const carMongooseSchema = new Schema<ICar>(
+  {
+    status: Boolean,
+    model: String,
+    year: Number,
+    color: String,
+    buyValue: Number,
+    doorsQty: Number,
+    seatsQty: Number,
+  },
+  { versionKey: false },
+);
 
 export default class CarModel extends MongoModel<ICar> {
   constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
--- src/models/Motorcycle.model.ts
+++ src/models/Motorcycle.model.ts
@@ -2,15 +2,18 @@ import { model as mongooseCreateModel, Schema } from 'mongoose';
 import { IMotorcycle } from '../interfaces/IMotorcycle';
 import MongoModel from './Mongo.model';
 
-const carMongooseSchema = new Schema<IMotorcycle>({
-  status: Boolean,
-  model: String,
-  year: Number,
-  color: String,
-  buyValue: Number,
-  category: String,
-  engineCapacity: Number,
-});
+const carMongooseSchema = new Schema<IMotorcycle>(
+  {
+    status: Boolean,
+    model: String,
+    year: Number,
+    color: String,
+    buyValue: Number,
+    category: String,
+    engineCapacity: Number,
+  },
+  { versionKey: false },
+);
 
 export default class MotorcycleModel extends MongoModel<IMotorcycle> {
   constructor(model = mongooseCreateModel('Motorcycle', carMongooseSchema)) {
